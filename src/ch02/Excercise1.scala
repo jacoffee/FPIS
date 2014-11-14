5ec0c725b70c8666cdc5febb847d5643676d6d11
allen <allen@qiaobutang.com>
2014-11-14 10:27:11 +0800

    finish first task, gonna say tail-recursion begin to show its advantage

--- src/ch02/Excercise1.scala
+++ src/ch02/Excercise1.scala
@@ -1,5 +1,6 @@
 package ch02
 
+import scala.annotation.tailrec
 /**
  * Created by allen on 14-11-13.
 	EXERCISE 1 (optional):
@@ -8,16 +9,52 @@ package ch02
 	Your definition should use a local tail-recursive function.
 
  */
+// 1、1、2、3、5、8、13、21
+// F0=0，F1=1，Fn=F(n-1)+F(n-2)（n>=2，n∈N*）
 
 object Excercise1 extends App {
-	// // Scala compiles the recursion to iterative loops that do not consume call stack frames for each iteration
+	// Scala compiles the recursion to iterative loops that do not consume call stack frames for each iteration
 
 	// simple and ugly without tail recursion option
-	// n should be bigger than one
-	def simpleFibonacci(n: Int): Int = {
-		 if (n >= 1 && n <= 2) n -1
-		else simpleFibonacci(n-1) + simpleFibonacci(n-2)
+	def simpleRecursion(n: Long): Long = {
+		 if (n >= 1 && n <= 2) 1
+		else simpleRecursion(n-1) + simpleRecursion(n-2)
 	}
 
-	def fib(n: Int): Int = ???
+	// iteration method
+	// an = an-1 + an-2
+	// next round the an-1 <- an; an-2<- an-1
+	def iteration(n: Int): Long = { // 1000 -> 60ms
+		if (n <= 1) 1
+		else {
+			var sum: Long = 0L
+			var prev = 0L
+			var cur = 1L
+			for (num <- 2 to n ) {
+				sum = prev + cur
+				prev = cur
+				cur = sum
+			}
+			sum
+		}
+	}
+
+	def tailRecursion(n: Long): Long = { // 1000 -> 51
+		@tailrec def loop(n: Long, prev: Long, cur: Long): Long = {
+			if (n < 1) prev
+			else loop(n-1, cur, cur+prev)
+			// 由于函数式编程 强调输入 | 输出， 所以每次完成两数之后
+			// 之前的后一个 变成了 下次计算的 prev
+			// 而前一次的 则变成了 下一次的当前值
+		}
+		loop(n, 0, 1)
+	}
+
+	// compare efficiency
+	val start  = System.currentTimeMillis
+	val r1 = tailRecursion(1000)
+	println(" time consuming " + (System.currentTimeMillis -start))
+	println(" result " + r1)
+	//Integer.MAX_VALUE
+	//Long.MaxValue
 }
