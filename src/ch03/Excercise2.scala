ec4160802dbe769281a877e25bac8fc254b04990
allen <allen@qiaobutang.com>
2014-12-04 14:47:07 +0800

    waiting to get tree down

--- src/ch03/Excercise2.scala
+++ src/ch03/Excercise2.scala
@@ -5,9 +5,14 @@ package ch03
  */
 
 object Excercise2 extends App {
-	import ch03.Tree.{ size, maxNum, map }
+	import ch03.Tree.{ size, maxNum, map, size1, maxNum1, size3 }
 	val newTree = Branch[Int](Leaf(1), Branch[Int](Leaf(3), Branch[Int](Leaf(4), Leaf(56))))
 	println(" size " + size(newTree))
+	println(" size1 " + size1(newTree))
 	println(" maxNum " + maxNum(newTree))
+	println(" maxNum1  " + maxNum1(newTree))
 	println(" map " + map(newTree)(_ + 1))
+
+
+	println(" size3 " +  size3(newTree))
 }
--- src/ch03/Tree.scala
+++ src/ch03/Tree.scala
@@ -31,6 +31,13 @@ object Tree {
 		go(tree)
 	}
 
+	def size1[A](tree: Tree[A]): Int = {
+		tree match {
+			case Leaf(v) => 1
+			case Branch(left, right) => size(left) + size(right)
+		}
+	}
+
 	// EXERCISE 26: Write a function that maximum returns the maximum element in a Tree[Int]。
 	def maxNum(t: Tree[Int]): Int= {
 		import scala.math.max
@@ -46,28 +53,48 @@ object Tree {
 		go(t)
 	}
 
+	def maxNum1(tree: Tree[Int]): Int= {
+		tree match {
+			case Leaf(v) => max(Int.MinValue, v)
+			case Branch(left, right) => maxNum1(left) max maxNum1(right)
+		}
+	}
+
 	// EXERCISE 27: Write a function depth that returns the maximum path length from the root of a tree to any leaf
-	def maxDepth[A](t: Tree[A]): Int = {
+	def maxDepth[A](tree: Tree[A]): Int = {
 		3
 	}
 
 	// val newTree = Branch[Int](Leaf(1), Branch[Int](Leaf(3), Branch[Int](Leaf(4), Leaf(56))))
-	def map[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+	def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
 		// 调用之后 再拼接
 		// The following writing lose its original structure, if we want to reuse the originla Structure, we have to recurse over the previous structure
-		def go(tree: Tree[A])(f: A => B): Tree[B]= {
-			tree match {
-				case Leaf(v) => Leaf(f(v))
-				case Branch(left, right) => {
-					go(left)(f)
-					go(right)(f)
+		/*
+			def go(tree: Tree[A])(f: A => B): Tree[B]= {
+				tree match {
+					case Leaf(v) => Leaf(f(v))
+					case Branch(left, right) => {
+						go(left)(f)
+						go(right)(f)
+					}
 				}
 			}
-		}
-		t match {
+		*/
+		tree match {
 			case Leaf(v) => Leaf(f(v))
 			case Branch(left, right) => Branch(map(left)(f), map(right)(f))
 		}
+	}
 
+	// EXERCISE 29: Generalize size, maximum, depth, map writing a new function that abstracts over their similarities.
+	// Int => Leaf(5)  max Leaf
+	def size3(tree: Tree[Int]) = fold(tree, 1)((_, acc) => acc + 1)
+	def fold[A, B](tree: Tree[A], z: B)(f: (A, B) => B): Tree[B] = {
+		tree match {
+			case Leaf(v) => Leaf(f(v, z))
+			case Branch(left, right) => Branch(
+				fold(left, z)(f), fold(right, z)(f)
+			)
+		}
 	}
 }
